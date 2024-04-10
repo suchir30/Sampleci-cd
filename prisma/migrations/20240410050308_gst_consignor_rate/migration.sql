050cd785cee85b1f1c9fda07e2bcf318b62870a2
rbvroomster <151752425+rbvroomster@users.noreply.github.com>
2024-04-10 10:39:21 +0530

    Re-generated migration files for GST and Consignor Rate (#17)

--- prisma/migrations/20240410050308_gst_consignor_rate/migration.sql
+++ prisma/migrations/20240410050308_gst_consignor_rate/migration.sql
@@ -1,5 +1,8 @@
 -- AlterTable
-ALTER TABLE `consignee` MODIFY `consigneeCode` VARCHAR(255) NULL;
+ALTER TABLE `Consignee` MODIFY `consigneeCode` VARCHAR(255) NULL;
+
+-- AlterTable
+ALTER TABLE `Consignor` ADD COLUMN `gstId` INTEGER NULL;
 
 -- CreateTable
 CREATE TABLE `ConsignorRateTable` (
@@ -13,6 +16,20 @@ CREATE TABLE `ConsignorRateTable` (
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `GstMaster` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
+    `createdOn` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `GstMaster_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Consignor` ADD CONSTRAINT `Consignor_gstId_fkey` FOREIGN KEY (`gstId`) REFERENCES `GstMaster`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `ConsignorRateTable` ADD CONSTRAINT `ConsignorRateTable_consignorId_fkey` FOREIGN KEY (`consignorId`) REFERENCES `Consignor`(`consignorId`) ON DELETE RESTRICT ON UPDATE CASCADE;
